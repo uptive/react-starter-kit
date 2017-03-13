d67cc05f3738ef18c154b276a30ea72ad77906ad
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-03-13 13:54:06 +0100

    changed home.js to a react component with state

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,45 +1,73 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Layout from '../../components/Layout';
 import s from './Home.css';
 import { Button } from 'react-bootstrap';
 
-function Home({ loggedInUser }) {
-  var content = renderContent(loggedInUser);
-  return (
-    <Layout>
-      <div className={s.container}>
-        <h1 className={s.heading}>Welcome to Uptiverse</h1>
-        { content }
+/**/
+class Home extends Component {
+
+  static contextTypes = {
+      store: React.PropTypes.object
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state =  {
+      loggedInUser: this.context.store.getState().user
+    };
+    this.changeHandler = this.context.store.subscribe(() => this.handleChange(this));
+  }
+
+  componentWillUnmount(){
+    if(this.changeHandler){
+      this.changeHandler();
+    }
+  }
+
+  handleChange(){
+    this.setState({
+      loggedInUser: this.context.store.getState().user
+    });
+  }
+
+  render() {
+    var content = this.renderContent(this.state.loggedInUser);
+    return (
+      <Layout>
+        <div className={s.container}>
+          <h1 className={s.heading}>Welcome to Uptiverse</h1>
+          { content }
+        </div>
+      </Layout>
+    );
+  }
+
+  renderContent(loggedInUser){
+    if(loggedInUser){ return this.renderPrivateContent(); }
+    return this.renderPublicContent();
+  }
+
+  renderPrivateContent() {
+    return (
+      <div>
+        <p className={s.description}>This is where the magic happens and you are good to go!</p>
       </div>
-    </Layout>
-  );
-}
+    );
+  }
 
-function renderContent(loggedInUser){
-  if(loggedInUser){ return renderPrivateContent(); }
-  return renderPublicContent();
-}
-
-function renderPrivateContent(){
-  return (
-    <div>
-      <p className={s.description}>This is where the magic happens and you are good to go!</p>
-    </div>
-  );
-}
-
-function renderPublicContent(){
-  return (
-    <div>
-      <p className={s.description}>This is where the magic happens and in order to be a part of the magic you need to login. <br/> Use your Uptive account to sign in.</p>
-      <div className={s.buttonContainer}>
-        <Button className={s.loginButton} href="/login/google">Sign in</Button>
+  renderPublicContent() {
+    return (
+      <div>
+        <p className={s.description}>This is where the magic happens and in order to be a part of the magic you need to login. <br/> Use your Uptive account to sign in.</p>
+        <div className={s.buttonContainer}>
+          <Button className={s.loginButton} href="/login/google">Sign in</Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 /*
 <p>- or -</p>
 <a href=""> Sign in with token</a>
