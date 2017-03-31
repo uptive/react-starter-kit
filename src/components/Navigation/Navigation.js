2322dda1fd13cd11fd6d6e00c7956bb6a496acff
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-03-31 17:45:00 +0200

    reworked menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,39 +5,86 @@ import s from './Navigation.css';
 import Link from '../Link';
 import { logout } from '../../actions/logout';
 import { Popover, OverlayTrigger } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 
-function Navigation({ className }, context ) {
-  return (
-    <div className={cx(s.root, className)} role="navigation">
-      { getContentBasedOnLoggedInUser(context.store.getState().user) }
-    </div>
-  );
+class Navigation extends React.Component {
+  static propTypes = {
+    className: PropTypes.string,
+  };
+
+  static contextTypes = {
+      store: React.PropTypes.object
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      showMenu: false,
+    };
+  }
+  render() {
+    return (
+      <div className={cx(s.root, this.props.className)} role="navigation">
+        { this.renderIfLoggedin(this.context.store.getState().user) }
+      </div>
+    );
+  }
+
+  renderIfLoggedin(user){
+    if(!this.isLoggedIn(user)) return null;
+    return (
+      <div>
+        <span className={cx(s.link, s.highlight)} onClick={(e)=>{ this.toggleMenu(e)}}><FontAwesome name='bars'/></span>
+        {this.renderOpenMenu()}
+      </div> );
+  }
+
+  renderOpenMenu(){
+    if(!this.state.showMenu){ return null;}
+    return (
+      <div className={s.menuContainer}>
+        <div className={s.menuContent}>
+          <Link className={s.menuBlockLink} to="/news">
+            <div className={s.menuBlock}>
+              <FontAwesome className={s.menuIcon} name='newspaper-o'/>
+              <span className={s.menuText}>News</span>
+            </div>
+          </Link>
+
+          <Link className={s.menuBlockLink} to="/employees">
+            <div className={s.menuBlock}>
+              <FontAwesome className={s.menuIcon} name='users'/>
+              <span className={s.menuText}>Employees</span>
+            </div>
+          </Link>
+
+          <Link className={s.menuBlockLink} to="/recruits">
+            <div className={s.menuBlock}>
+              <FontAwesome className={s.menuIcon} name='user-plus'/>
+              <span className={s.menuText}>Recruits</span>
+            </div>
+          </Link>
+
+          <span className={s.menuBlockLink} onClick={ logout() }>
+            <div className={s.menuBlock}>
+              <FontAwesome className={s.menuIcon} name='sign-out'/>
+              <span className={s.menuText}>Logout</span>
+            </div>
+          </span>
+          </div>
+      </div>
+    );
+  }
+
+  isLoggedIn(user){
+    if(user){ return true; }
+    return false;
+  }
+
+  toggleMenu(){
+    this.setState({showMenu: !this.state.showMenu});
+  }
+
 }
-
-function getContentBasedOnLoggedInUser(user){
-  if(!isLoggedIn(user)) return null;
-//<MenuPresentation />
-  return (
-    <div>
-      <Link className={s.link} to="/news">News</Link>
-      <span className={s.spacer}> | </span>
-      <Link className={s.link} to="/employees">Employees</Link>
-      <span className={s.spacer}> | </span>
-      <span className={s.link} onClick={ logout() }>Logout</span>
-    </div> );
-}
-
-function isLoggedIn(user){
-  if(user){ return true; }
-  return false;
-}
-
-Navigation.propTypes = {
-  className: PropTypes.string,
-};
-
-Navigation.contextTypes = {
-    store: React.PropTypes.object
-};
 
 export default withStyles(s)(Navigation);
